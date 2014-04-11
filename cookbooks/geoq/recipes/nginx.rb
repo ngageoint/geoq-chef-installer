fc6d97ca836f96b56ab20fd874e02da7e79c8cea
Tyler Garner <garnertb@gmail.com>
2014-04-11 07:46:10 -0400

    Use node.default vs node.set to specify default attributes in the nginx/postgresql cookbooks.

--- cookbooks/geoq/recipes/nginx.rb
+++ cookbooks/geoq/recipes/nginx.rb
@@ -1,11 +1,11 @@
-node.set['nginx']['init_style'] = "init"
-node.set['nginx']['install_method'] = 'source'
-node.set['nginx']['source']['version'] = '1.4.1'
-node.set['nginx']['source']['url'] = "http://nginx.org/download/nginx-#{node['nginx']['source']['version']}.tar.gz"
-node.set['nginx']['source']['prefix']                  = "/opt/nginx-#{node['nginx']['source']['version']}"
-node.set['nginx']['source']['conf_path']               = "#{node['nginx']['dir']}/nginx.conf"
-node.set['nginx']['source']['sbin_path']               = "#{node['nginx']['source']['prefix']}/sbin/nginx"
-node.set['nginx']['source']['default_configure_flags'] = [
+node.default['nginx']['init_style'] = "init"
+node.default['nginx']['install_method'] = 'source'
+node.default['nginx']['source']['version'] = '1.4.1'
+node.default['nginx']['source']['url'] = "http://nginx.org/download/nginx-#{node['nginx']['source']['version']}.tar.gz"
+node.default['nginx']['source']['prefix']                  = "/opt/nginx-#{node['nginx']['source']['version']}"
+node.default['nginx']['source']['conf_path']               = "#{node['nginx']['dir']}/nginx.conf"
+node.default['nginx']['source']['sbin_path']               = "#{node['nginx']['source']['prefix']}/sbin/nginx"
+node.default['nginx']['source']['default_configure_flags'] = [
   "--prefix=#{node['nginx']['source']['prefix']}",
   "--conf-path=#{node['nginx']['dir']}/nginx.conf",
   "--sbin-path=#{node['nginx']['source']['sbin_path']}"
@@ -14,7 +14,7 @@ node.set['nginx']['source']['default_configure_flags'] = [
 # Set sendfile to off when in development mode.
 # http://smotko.si/nginx-static-file-problem/
 if node['geoq']['debug']
-    node.set['nginx']['sendfile'] = 'off'
+    node.default['nginx']['sendfile'] = 'off'
 end
 
 include_recipe 'nginx::default'
