6719bc389aa2e396adbb731b48518f03f3eaf981
Khazhismel Kumykov <kkumykov@mitre.org>
2015-05-15 15:45:59 -0400

    Use ruby 2.1 as 2.2 breaks compatability with older json gems

--- scripts/install_rvm.sh
+++ scripts/install_rvm.sh
@@ -3,4 +3,4 @@
 sudo apt-get install -y curl
 
 sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 # RVM installer key
-curl -sSL https://get.rvm.io | bash -s stable --ruby
+curl -sSL https://get.rvm.io | bash -s stable --ruby=2.1
