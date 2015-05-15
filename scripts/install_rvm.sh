775617d57e6fb9df8f99913183fc1ab67016e063
Khazhismel Kumykov <kkumykov@mitre.org>
2015-05-15 14:29:38 -0400

    import RVM signing key before installing RVM. Should fix ngageoint/geoq-chef-installer#5

--- scripts/install_rvm.sh
+++ scripts/install_rvm.sh
@@ -2,4 +2,5 @@
 
 sudo apt-get install -y curl
 
+sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 # RVM installer key
 curl -sSL https://get.rvm.io | bash -s stable --ruby
